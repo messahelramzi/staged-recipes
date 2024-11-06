56b52cc13ab3fb70326c06457089e01689ff1440
Ramzi MESSAHEL <ramzi.messahel@safrangroup.com>
2024-11-06 15:15:17 +0100

    Uprage to ArborX v1.7 and add commands in test stage

--- recipes/arborx/bld.bat
+++ recipes/arborx/bld.bat
@@ -3,7 +3,8 @@ setlocal EnableDelayedExpansion
 mkdir build
 if errorlevel 1 exit 1
 cmake . ^
-    -D CMAKE_INSTALL_PREFIX=$PREFIX$ 
+    -D CMAKE_INSTALL_PREFIX=$PREFIX$ ^
+    -D CMAKE_CXX_EXTENSIONS=OFF ^
     -S %SRC_DIR% ^
     -B build ^
     -D ARBORX_ENABLE_MPI=OFF ^
--- recipes/arborx/build.sh
+++ recipes/arborx/build.sh
@@ -3,9 +3,11 @@ set -ex
 cmake . \
     -B build \
     -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -D CMAKE_CXX_EXTENSIONS= OFF \
     -D ARBORX_ENABLE_MPI=OFF \
     ${CUDA_ARGS} # only for CUDA-enabled Kokkos
 
 cd build
 make -j1 # to have consistent logging
+
 make install
